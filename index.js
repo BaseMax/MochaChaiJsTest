c4510924f74c15baf04d620c71ef551cdb229717
Max Base <maxbasecode@gmail.com>
2022-07-31 20:09:47 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    sayHello: () => {
+        return 'Hello, World!';
+    },
+    concat: (value1, value2) => {
+        return value1 + "" + value2;
+    },
+    addNumbers: (value1, value2) => {
+        return value1 + value2;
+    }
+};
